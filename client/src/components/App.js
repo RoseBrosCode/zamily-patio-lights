51e76f8c4d476cf16ecb63adb6208103309dad47
Zach Rose <zwrose@gmail.com>
2020-08-19 18:01:28 -0400

    repo cleanup and baseline LightsOnOff implementation

---
+++ client/src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import LightsOnOff from 'components/LightsOnOff';
+
+function App() {
+  return (
+    // <> is shorthand for declaring a React Fragment https://reactjs.org/docs/fragments.html#short-syntax
+    <>
+      <CssBaseline />
+      <h1>Zamily Patio Controller</h1>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={6}>
+          <Paper>
+            <LightsOnOff />
+          </Paper>
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <Paper>Grill Lights Controls</Paper>
+        </Grid>
+      </Grid>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/LightsOnOff.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+
+
+class LightsOnOff extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      stringsOn: false,
+      grillOn: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    if (e.currentTarget.name === 'strings') {
+      this.setState({
+        stringsOn: true,
+        grillOn: false
+      });
+
+    } else if (e.currentTarget.name === 'grill') {
+      this.setState({
+        stringsOn: false,
+        grillOn: true
+      });
+
+    } else if (e.currentTarget.name === 'bothOn' ) {
+      this.setState({
+        stringsOn: true,
+        grillOn: true
+      });
+
+    } else if (e.currentTarget.name === 'bothOff') {
+      this.setState({
+        stringsOn: false,
+        grillOn: false
+      });
+
+    } else {
+      console.log('Unexpected event...', e)
+    }
+  }
+
+  render() { 
+    console.log('current state: ', this.state);
+    return (
+      <div>
+        <h2>Lights On/Off</h2>
+        <br />
+        <Button
+          name="strings"
+          variant="contained"
+          color={this.state.stringsOn && !this.state.grillOn ? 'primary' : 'default'}
+          onClick={this.handleClick}
+        >
+          Strings Only
+        </Button>
+        <Button
+          name="grill"
+          variant="contained"
+          color={!this.state.stringsOn && this.state.grillOn ? 'primary' : 'default'}
+          onClick={this.handleClick}
+        >
+          Grill Only
+        </Button>
+        <Button
+          name="bothOn"
+          variant="contained"
+          color={this.state.stringsOn && this.state.grillOn ? 'primary' : 'default'}
+          onClick={this.handleClick}
+        >
+          Both On
+        </Button>
+        <Button
+          name="bothOff"
+          variant="contained"
+          color={!this.state.stringsOn && !this.state.grillOn ? 'primary' : 'default'}
+          onClick={this.handleClick}
+        >
+          Both Off
+        </Button>
+      </div>
+    )
+  }
+}
+
+export default LightsOnOff; 
