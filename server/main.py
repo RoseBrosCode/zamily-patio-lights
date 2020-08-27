dafaff0b56f9eff417f9e9a00c9979e4419c3176
Ryan Rose <rytrose@gmail.com>
2020-08-26 21:31:05 -0400

    switch to npm from yarn, solidify dockerfile

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__, static_folder='../client/build', static_url_path='/')
 
@@ -7,7 +7,30 @@ def index():
     """Serves the HTML entrypoint to the React app."""
     return app.send_static_file('index.html')
 
+
+@app.route('/state', methods=['GET'])
+def get_state():
+    return jsonify({
+        "stringsOn": False,
+        "grillOn": False,
+        "animation": 1,
+        "red": 252,
+        "green": 101,
+        "blue": 20,
+        "speed": 5,
+        "direction": 0,
+        "density": 0.5,
+        "tailLength": 250
+    }), 200
+
+
 @app.route('/animation', methods=['POST'])
 def update_animation_state():
     app.logger.info(f"animation update: {request.json}")
+    return '', 200
+
+
+@app.route('/lights', methods=['POST'])
+def update_lights_state():
+    app.logger.info(f"lights update: {request.json}")
     return '', 200
