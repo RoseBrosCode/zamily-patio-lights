007877e5999a5a16dc9503959e39be0b93cdcc40
Zach Rose <zwrose@gmail.com>
2023-01-29 13:34:23 -0500

    gcp attempt due to weird local docker issues

--- server/constants.py
+++ server/constants.py
@@ -2,6 +2,10 @@ from enum import Enum
 
 FLASK_NAME = "ZAMILY_PATIO_LIGHTS"
 
+HA_URL = "https://0e6v41nox33cfu81evaxlks9dfmm97wc.ui.nabu.casa/api"
+HA_PATIO_STRINGS_ID = "light.lights_group_patio_string_lights"
+HA_GRILL_LIGHTS_ID = "light.patio_grill_lights_outlet"
+
 class AnimationType(Enum):
     WARM = 0
     COLOR = 1
